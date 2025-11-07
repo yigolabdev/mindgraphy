2b7495754727ac919fee83cb158288a9c8c99d9c
Your Name <riz2019247@gmail.comm>
2025-11-07 12:58:16 +0900

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'export', // ✅ enable static HTML export
+  images: {
+    unoptimized: true, // optional: allows <Image> to work without Image Optimization API
+  },
 };
+
 
 export default nextConfig;
